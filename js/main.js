f461b3dcf659c672c99b96404a4b6b2f63f8db9e
syamaktaheri <sya@localhost.localdomain>
2022-02-13 15:33:43 +0330

    create navigation bar and hero component

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const collapsibles = document.querySelectorAll(".collapse");
+const menu = document.querySelectorAll(".navBar");
+
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapse--expanded");
+  })
+);
+
+menu.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("list--expanded");
   })
 );
